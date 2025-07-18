b9beef5f53d1c04bdaf993312b46795514ecaf21
sanjay-shan08 <sanjayshanmuganandan@gmail.com>
2025-07-18 13:59:33 +0000

    final commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const display = document.getElementById('display');
+
+function appendValue(val) {
+    display.value += val;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    }
+    catch {
+        display.value = 'Error';
+    }
+}
